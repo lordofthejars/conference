e10f9bbaa5ccd2fcfc3f87c53aeef481937cf16d
Alex Soto <asotobu@gmail.com>
2017-11-01 10:25:40 +0100

    Initial

---
+++ src/main/java/org/conference/model/Speaker.java
@@ -0,0 +1,114 @@
+package org.conference.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Speaker implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+	@Version
+	@Column(name = "version")
+	private int version;
+
+	@Column
+	@NotNull
+	private String firstname;
+
+	@Column
+	@NotNull
+	private String surname;
+
+	@Column
+	private String twitter;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(final int version) {
+		this.version = version;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Speaker)) {
+			return false;
+		}
+		Speaker other = (Speaker) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getTwitter() {
+		return twitter;
+	}
+
+	public void setTwitter(String twitter) {
+		this.twitter = twitter;
+	}
+
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (id != null)
+			result += "id: " + id;
+		result += ", version: " + version;
+		if (firstname != null && !firstname.trim().isEmpty())
+			result += ", firstname: " + firstname;
+		if (surname != null && !surname.trim().isEmpty())
+			result += ", surname: " + surname;
+		if (twitter != null && !twitter.trim().isEmpty())
+			result += ", twitter: " + twitter;
+		return result;
+	}
+}
